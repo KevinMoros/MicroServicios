70387fcd37ec690fafd75321fc278a6b3b95f9c0
KevinMoros <kevinoliver.toro@viewnext.com>
2024-05-30 09:15:18 +0200

    Microservicios de Libros que utiliza una base de datos.

---
+++ 03-librosCRUD-BD/src/main/java/com/curso/service/LibrosService.java
@@ -0,0 +1,19 @@
+package com.curso.service;
+
+import java.util.List;
+
+import com.curso.model.Libro;
+
+public interface LibrosService {
+
+	List<Libro> listaLibros();
+
+	Libro buscarLibro(int isbn);
+
+	void altaLibro(Libro libro);
+
+	void actualizarLibro(Libro libro);
+
+	List<Libro> eliminarLibro(int isbn);
+
+}
